1995044a6c1437b0cc80512f86a32b19e4576375
Jacob Lee <leesa6@oregonstate.edu>
2021-05-11 18:39:01 -0700

    First Commit for testing

---
+++ task.py
@@ -0,0 +1,3 @@
+def my_func():
+	return "Hello World"
+
---
+++ tests.py
@@ -0,0 +1,16 @@
+import unittest
+import task
+
+
+class TestCase(unittest.TestCase):
+	def test1(self):
+	expected = "Hello World"
+	self.assertEqual(task.my_func(), expected)
+
+	def test2(self):
+	self.assertEqual(task.my_func(), expected)
+
+
+if __main__ == '__main__':
+	unittest.main()
+
